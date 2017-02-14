e2a9fac9dfad7c0ee01ccc9ead8ab8c63010d564
01shruti <shruti.goyal94@gmail.com>
2017-02-14 12:59:17 +0000

    updated code

--- Descriptive_stats.R
+++ Descriptive_stats.R
@@ -22,25 +22,3 @@ print(round(corr1,digits=3))
 
 pairs(df,panel = points,main="Correlation plot among variables of dataset")
 ######################################################################################
-
-reg1 <- lm(df$EDUC~df$HVAL150)
-par(cex=0.8)
-plot(df$EDUC,df$HVAL150)
-abline(reg1)
-
-reg2 <- lm(df$AGE60~df$EDUC)
-par(cex=0.9)
-plot(df$AGE60,df$EDUC)
-abline(reg2)
-
-######################################################################################
-hist(df$AGE60, xlab = "%age of population aged 60 or older",main="Histogram of AGE60")
-######################################################################################
-
-library(bayesm)
-store <- levels(as.factor(oj$store))
-n <- length(unique(store))
-ldata = NULL
-
-mcmc = list(R=2000,keep=15)
-out = rhierMnlRwMixture(Data = list(p=4,lgtdata = df),Prior=list(ncomp=1),Mcmc= mcmc)
